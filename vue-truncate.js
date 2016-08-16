bc966ef5f16b5e6fdb45371fce6ab20973e6c512
imcvampire <quocanh96@gmail.com>
2016-08-16 09:57:37 +0700

    Add vue-truncate

---
+++ vue-truncate.js
@@ -0,0 +1,19 @@
+(function () {
+
+  function install (Vue) {
+	  Vue.filter('truncate', function (value, length) {
+      return value.length > length 
+        ? value.slice(value, length - 3) + '...'
+        : value
+	  })
+  }
+
+  if (typeof exports == "object") {
+    module.exports = install
+  } else if (typeof define == "function" && define.amd) {
+    define([], function(){ return install })
+  } else if (window.Vue) {
+    Vue.use(install)
+  }
+
+})()
