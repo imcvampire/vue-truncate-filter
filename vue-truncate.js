7e81057eb3cb368308eec86c470625ebe860908c
Nguyen Quoc Anh <quocanh96@gmail.com>
2016-09-11 11:24:25 +0700

    restyle

--- vue-truncate.js
+++ vue-truncate.js
@@ -1,6 +1,6 @@
 ;(function () {
 
-  var vueTruncate = {}
+  var vueTruncate = {};
 
   vueTruncate.install = function (Vue) {
     
@@ -13,21 +13,21 @@
      */
 
     Vue.filter('truncate', function (text, length, clamp) {
-      clamp = clamp || '...'
+      clamp = clamp || '...';
 
       return text.length > length 
         ? text.slice(0, length - clamp.length) + clamp
         : text
-	  })
+      });
   }
 
   if (typeof exports == "object") {
-    module.exports = vueTruncate
+    module.exports = vueTruncate;
   } else if (typeof define == "function" && define.amd) {
-    define([], function(){ return vueTruncate })
+    define([], function(){ return vueTruncate });
   } else if (window.Vue) {
-    window.VueTruncate = vueTruncate
-    Vue.use(VueTruncate)
+    window.VueTruncate = vueTruncate;
+    Vue.use(VueTruncate);
   }
 
 })()
