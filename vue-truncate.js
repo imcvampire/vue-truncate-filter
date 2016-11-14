64e8b1365dea1bf0d5a52f7e875b50eba943cf07
imcvampire <quocanh96@gmail.com>
2016-11-14 15:05:03 +0700

    fix bug: lack case

--- vue-truncate.js
+++ vue-truncate.js
@@ -15,16 +15,19 @@
     Vue.filter('truncate', function (text, length, clamp) {
       clamp = clamp || '...';
       length = length || 30;
+      
+      if (text.length < length) return text;
 
       var tcText = text.slice(0, length - clamp.length);
       var last = tcText.length - 1;
+      
 
       while (last > 0 && tcText[last] !== ' ' && tcText[last] !== clamp[0])
 	last -= 1;
 
       tcText =  tcText.slice(0, last);
 
-      return tcText + (text.length > length ? clamp : '');
+      return tcText + clamp;
     });
   }
 
